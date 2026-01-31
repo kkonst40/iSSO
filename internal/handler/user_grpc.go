32ff1b3fd89b5ce905559305c339a64359596840
kkonst40 <161722238+kkonst40@users.noreply.github.com>
2026-01-31 20:05:05 +0300

    gRPC "Exist" method added

---
+++ internal/handler/user_grpc.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	pb "github.com/kkonst40/isso/internal/gen/user"
+	"github.com/kkonst40/isso/internal/service"
+)
+
+type UserGRPCHandler struct {
+	pb.UnimplementedUserServiceServer
+	userService *service.UserService
+}
+
+func NewUserGRPCHandler(userService *service.UserService) *UserGRPCHandler {
+	return &UserGRPCHandler{userService: userService}
+}
+
+func (s *UserGRPCHandler) Exist(ctx context.Context, req *pb.ExistRequest) (*pb.ExistResponse, error) {
+	var inputIDs []uuid.UUID
+	for _, idStr := range req.Ids {
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			continue
+		}
+		inputIDs = append(inputIDs, id)
+	}
+
+	existingIDs, err := s.userService.Exist(ctx, inputIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]string, len(existingIDs))
+	for i, id := range existingIDs {
+		result[i] = id.String()
+	}
+
+	return &pb.ExistResponse{ExistingIds: result}, nil
+}
